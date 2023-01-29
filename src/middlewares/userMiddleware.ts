0904cb4a410f62d247907c80a3cf91ce298a3c7b
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-29 19:43:40 -0300

    feat: user middleware

---
+++ src/middlewares/userMiddleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { userSchema } from "../schemas/userSchemas.js";
+import { User } from "../protocols/User.js";
+
+
+export function checkMovieObj (req: Request, res: Response, next: NextFunction) {
+    const user = req.body as User;
+
+
+    if (!user) {
+        return res.sendStatus(400);
+    }
+
+    const { error } = userSchema.validate(user, { abortEarly: false });
+
+    if (error) {
+        const errors = error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    res.locals.user = user;
+    next();
+}
